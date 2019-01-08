88bfd00062f9693b5992479329612b23b9213c1f
Ahyer <zuixianlingfu@gmail.com>
2019-01-08 15:23:04 +0800

    更新了LRU算法

--- Os/Form1.cs
+++ Os/Form1.cs
@@ -17,6 +17,7 @@ namespace Os
     {
 
         private Thread Tfifo = null;
+        private Thread Tlru = null;
         private ArrayList list = new ArrayList(); 
         public Form1()
         {
@@ -37,8 +38,6 @@ namespace Os
             string a = Request.xulie;
             /*
             这个for循环是一个实验性质的，存在问题。比如默认了第一个字母为页面号；
-
-
             */
             int max = Request.NumsOfneicun;
             int i;
@@ -52,6 +51,8 @@ namespace Os
             list.Add(a[i * 6]);
             return result;
         }
+
+        //左上角的设置选项。
         private void 设置ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Setting set = new Setting();
@@ -59,10 +60,10 @@ namespace Os
             
         }
 
-
+        //每一哥算法开始前，都应该进行一次restart操作。
         private void restart()
         {
-            int kuaishu = Request.NumsOfwulikuai;
+            
             list.Clear();
             change();
            
@@ -83,6 +84,8 @@ namespace Os
         }
         private void FIFOMethod()
         {
+
+            //这个q是物理块的队列。
             Queue<int> q = new Queue<int>();
 
             int wulikuai = Request.NumsOfwulikuai;
@@ -90,15 +93,17 @@ namespace Os
           
             for (int i = 0; i <= list.Count; i++)//列数
             {
+                //设置是否缺页的标志
                   bool queye =false;
                 if (i != 0)
                 {
+                    //只能这样转换，否则会产生错误。
                     String a = Convert.ToString(list[i - 1]);
                     int b = Convert.ToInt32(a);
                     if (!q.Contains(b))
                     {
                         q.Enqueue(b);//加入队列
-                        queye = true;
+                        queye = true;//设置需要缺页中断的标志。
                     }
                    
                    
@@ -111,7 +116,7 @@ namespace Os
                     lab.Top = j * 30;
                     if (i==0)
                     {
-                        lab.Width = 60;//如果不声明这个宽度有问题！
+                        lab.Width = 60;//如果不声明这个宽度有问题！宽度还需要微调。
                         lab.Left = i * 60 + 50;
                         lab.Top = j * 30;
                         if (j==0)
@@ -147,8 +152,7 @@ namespace Os
                         lab.Width = 10;
                         lab.Left = i * 60 + 50;
                         lab.Top = j * 30;
-                     
-                    
+   
                         if (q.Count>Request.NumsOfwulikuai)
                         {
                             q.Dequeue();
@@ -190,11 +194,121 @@ namespace Os
             }
         }
      
+        private void LRUMethod()
+        {
+            LinkedList<int> q = new LinkedList<int>();
+ 
+            int wulikuai = Request.NumsOfwulikuai;
+            for (int i = 0; i <= list.Count; i++)
+            {
+                bool queye =true;
+                if (i!=0)
+                {
+                    String a =Convert.ToString( list[i - 1]);
+                    int b = Convert.ToInt32(a);
+                    
+                    if (q.Contains(b))
+                    {
+                        queye =false;
+                        q.Remove(b);
+                    }
+                    q.AddFirst(b);
+
+                }
+                for (int j = 0; j < wulikuai+2; j++)
+                {
+                    Label lab = new Label();
+                    lab.Left = 50;
+                    lab.Top = j * 30;
+                    if (i == 0)
+                    {
+                        lab.Width = 60;//如果不声明这个宽度有问题！宽度还需要微调。
+                        lab.Left = i * 60 + 50;
+                        lab.Top = j * 30;
+                        if (j == 0)
+                        {
+                            lab.Text = "访问序列";
+                            this.Invoke(new Action(() =>
+                            {
+                                LRUPanel.Controls.Add(lab);
+                            }
+                  ));
+                        }
+                        else if (j <= wulikuai)
+                        {
+                            lab.Text = "物理块" + j.ToString();
+                            this.Invoke(new Action(() =>
+                            {
+                                LRUPanel.Controls.Add(lab);
+                            }
+                  ));
+                        }
+                        else
+                        {
+                            lab.Text = "是否缺页";
+                            this.Invoke(new Action(() =>
+                            {
+                                LRUPanel.Controls.Add(lab);
+                            }
+                  ));
+                        }
+                    }
+                    else
+                    {
+                        lab.Width = 10;
+                        lab.Left = i * 60 + 50;
+                        lab.Top = j * 30;
+                        if (q.Count>wulikuai)
+                        {
+                            q.RemoveLast();
+                        }
+                        int[] num = q.ToArray();
+                        if (j==0)
+                        {
+                            lab.Text = Convert.ToString(list[i - 1]);
+                        }
+                        else if (j <= wulikuai)
+                        {
+                            if (j <= q.Count)
+                            {
+                                lab.Text = num[j - 1].ToString();
+                            }
+
+                        }
+                        else
+                        {
+                            if (queye)
+                            {
+                                lab.Text = "√";
+                            }
+                        }
+                        if (IsHandleCreated)
+                        {
+                            this.Invoke(new Action(() =>
+                            {
+                                LRUPanel.Controls.Add(lab);
+                            }
+                          ));
+                        }
+                    }
+
+                }
+            }
+
         
+
+
+        }
 
         private void button2_Click(object sender, EventArgs e)
         {
             LRUPanel.BringToFront();
+            restart();
+            if (Tlru==null||Tlru.ThreadState.ToString()!="Running")
+            {
+                Tlru = new Thread(LRUMethod);
+                Tlru.Start();
+            }
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
